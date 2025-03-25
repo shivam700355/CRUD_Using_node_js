a6f5b61537acfdc9837ed8d9e67e42b3fba1a05f
shivam700355 <smshivam700751@gmail.com>
2025-03-25 22:12:19 +0530

    add this file

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const path = require('path');
-
+var methodOverride = require('method-override')
+app.use(methodOverride('_method'))
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -55,4 +56,31 @@ app.get('/edit/:id', (req, res) => {
     }
 
     res.render('edit.ejs', { student: studentdata });
+});
+
+app.patch('/update/:id', (req, res) => {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const studentIndex = students.findIndex(student => student.id === id);
+    if (studentIndex !== -1) {
+        students[studentIndex].name = name || students[studentIndex].name;
+        students[studentIndex].email = email || students[studentIndex].email;
+        res.redirect('/')
+    } else {
+        res.status(404).json({ message: `Student with ID ${id} not found` });
+    }
+});
+
+app.delete('/delete/:id', (req, res) => {
+    const { id } = req.params;
+    const studentIndex = students.findIndex(student => student.id === id);
+
+    if (studentIndex !== -1) {
+        const deletedStudent = students.splice(studentIndex, 1);
+
+       
+        res.redirect('/');
+    } else {
+        res.status(404).json({ message: `Student with ID ${id} not found` });
+    }
 });
